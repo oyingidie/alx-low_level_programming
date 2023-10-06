3c55e36d8f8973728ee614117f76430466633b01
oyingidie <oyinkansoladebayo@gmail.com>
2023-10-05 21:29:37 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+ * main - data entry
+ *
+ * return: always 0 (success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
