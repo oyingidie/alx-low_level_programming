56457b72b084744eaab4931cdfb64804a8c7972c
oyingidie <oyinkansoladebayo@gmail.com>
2023-10-05 21:49:19 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - indicate start point for program
+ *
+ * printf - print desired string
+ *
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
