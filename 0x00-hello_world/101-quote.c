d490d7cce5898a2ca2186a707e19222bddef0cbd
oyingidie <oyinkansoladebayo@gmail.com>
2023-10-06 06:00:39 -0700

    UNIX is basically a simple OS...

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 /**
- * main - start program
+ * main - Print string to standard error
  *
  * Return: always 1 (FAILURE)
  */
@@ -9,6 +9,6 @@
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, \
-2015-10-19\n", 59);
+			2015-10-19\n", 59);
 	return (1);
 }
