d90674c3a44a1e6101f9c3092e30fbf8d302dd2d
oyingidie <oyinkansoladebayo@gmail.com>
2023-10-06 21:01:26 -0700

    I sometimes suffer from insomnia...

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Display inputted condition
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		putchar('\n');
+		return (0);
+}
