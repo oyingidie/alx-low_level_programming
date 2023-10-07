c9cc950c7621639d7ea8b49edd565c103bf2bbfa
oyingidie <oyinkansoladebayo@gmail.com>
2023-10-06 21:11:26 -0700

    I sometimes suffer from insomnia...

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	char c;
+	char i;
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
 		putchar('\n');
-		return (0);
+	return (0);
 }
