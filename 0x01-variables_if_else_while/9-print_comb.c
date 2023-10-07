d9bc3edeca7da04294f0de711f1f44def14e6f82
oyingidie <oyinkansoladebayo@gmail.com>
2023-10-06 21:38:13 -0700

    Patience, persistence, and perspiration... Unbeatable combo!

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Display characters based on set conditions
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
