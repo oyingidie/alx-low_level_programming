ed6e44c0076f3c21e5212a6605c90b99f014790d
oyingidie <oyinkansoladebayo@gmail.com>
2023-10-10 16:29:36 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Display the sum of two integers
+ * @x: First character in the sum
+ * @y: Second character in the sum
+ *
+ * Return: Value of the sum
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
