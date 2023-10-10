02db22d6ae5c1bb65ea80dc9339801cf9ff00d58
oyingidie <oyinkansoladebayo@gmail.com>
2023-10-10 16:36:11 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Display all natural numbers based on defined conditions
+ * @n: Number to commence from
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
+	}
+	else
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+	printf("98\n");
+}
