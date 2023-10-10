d6b25cb72ea27941bf1dea899bbb0d5f604f780f
oyingidie <oyinkansoladebayo@gmail.com>
2023-10-10 14:41:40 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Display characters based on set conditions
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
