90280ab934ddfc8ca92890dfb1062be4d15d4136
oyingidie <oyinkansoladebayo@gmail.com>
2023-10-24 02:29:56 -0700

    Noise is a buffer???

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,33 +9,33 @@
 
 void print_buffer(char *b, int size)
 {
-	int o, j, i;
+	int i, j, k;
 
-	o = 0;
+	i = 0;
 
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-	while (o < size)
+	while (i < size)
 	{
-		j = size - o < 10 ? size - o : 10;
-		printf("%08x: ", o);
-		for (i = 0; i < 10; i++)
+		j = size - i < 10 ? size - i : 10;
+		printf("%08x: ", i);
+		for (k = 0; k < 10; k++)
 		{
 			if (i < j)
-				printf("%02x", *(b + o + i));
+				printf("%02x", *(b + i + k));
 			else
 				printf("  ");
-			if (i % 2)
+			if (k % 2)
 			{
 				printf(" ");
 			}
 		}
-		for (i = 0; i < j; i++)
+		for (k = 0; k < j; k++)
 		{
-			int c = *(b + o + i);
+			int c = *(b + i + k);
 
 			if (c < 32 || c > 132)
 			{
@@ -44,6 +44,6 @@ void print_buffer(char *b, int size)
 			printf("%c", c);
 		}
 		printf("\n");
-		o += 10;
+		i += 10;
 	}
 }
