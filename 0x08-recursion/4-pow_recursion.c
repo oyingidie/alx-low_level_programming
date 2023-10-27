5247372d5dccb3eb3e89d2dd2c5b63a1fbaa69e5
oyingidie <oyinkansoladebayo@gmail.com>
2023-10-26 18:13:58 -0700

    Idea captive of a brain

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _pow_recursion - Calculates power of a number, one
+  * multiplication step at a time
+  * @x: Integer
+  * @y: Number of multiplication steps
+  *
+  * Return: Power, or -1
+  */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+        	return (-1);
+	
+	else if (y == 0)
+        	return (1);
+
+	else
+        	return (x * _pow_recursion(x, (y - 1)));
+
+	return (0);
+}
