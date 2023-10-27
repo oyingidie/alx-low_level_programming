f50cbf1ec1e5309bf5eb2a76e519fda0e84137a9
oyingidie <oyinkansoladebayo@gmail.com>
2023-10-26 20:09:22 -0700

    Inception. Say nothing yet...

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compares two strings
+ * @s1: String pointer
+ * @s2: Wildcard string pointer
+ *
+ * Return: Identical, or otherwise
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	return (0);
+}
