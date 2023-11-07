9a82657075f1b4f974e0090b8bfc0f6299a3dff2
oyingidie <oyinkansoladebayo@gmail.com>
2023-11-07 06:01:55 -0800

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Displays name of file it was compiled from
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
