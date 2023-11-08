f723407f52c8ba5962ceaf29b40243e9571c4f66
oyingidie <oyinkansoladebayo@gmail.com>
2023-11-07 19:59:40 -0800

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - Dog's info
+* @name: Name of the dog
+* @age: Age of the dog
+* @owner: Owner of the dog
+*
+* Description: Defines a new type called 'dog' that stores info about a dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
