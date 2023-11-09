2dbf7d9b56a56adf7adeec95bec90412a682de49
oyingidie <oyinkansoladebayo@gmail.com>
2023-11-09 06:15:18 -0800

    To be is to be...

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Displays variable number of numbers seperated
+ * by given seperator
+ * @separator: String to be printed between numbers
+ * @n: Number of integers passed to the function
+ * @...: Indicator for variable number of numbers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int i;
+
+	va_start(nums, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(nums, int));
+		if (i != (n - 1) && separator != NULL)
+		printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(nums);
+}
