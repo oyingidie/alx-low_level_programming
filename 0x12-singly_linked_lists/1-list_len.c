4c015bc5035bd38807109c6c2530ff004f16fe3b
oyingidie <oyinkansoladebayo@gmail.com>
2023-11-20 07:41:37 -0800

    List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - Iterates through linked list and increments a
+ * counter for each node encountered
+ * @h: Pointer to the head of the linked list
+ *
+ * Return: Number of elements in singly linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+	
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	
+	return (n);
+}
