18cbf9d5f9b9ac3307448e63adf5ab19dcc7f9ee
oyingidie <oyinkansoladebayo@gmail.com>
2023-11-22 06:10:21 -0800

    Sum list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,13 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Adds vlaue to each node to running total
+ * @head: Pointer to the head of a singly linked list
+ *
+ * Return: The sum of all the data of the linked list
+ */
+
 int sum_listint(listint_t *head)
 {
- int sum = 0;
- listint_t *temp = head;
+	int sum = 0;
+	listint_t *temp = head;
 
- while (temp)
- {
- sum += temp->n;
- temp = temp->next;
- }
+	while (temp)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
 
- return (sum);
+	return (sum);
 }
