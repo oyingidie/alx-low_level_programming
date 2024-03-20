b879974b83effb6fbd58ee48ec25fc0ac5fbe580
oyingidie <oyinkansoladebayo@gmail.com>
2024-03-20 14:22:58 -0700

    Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+return the perimeter of an island
+the 'grid' is a list of list of integers
+water zone is represented with 0 & land zone with 1
+"""
+def island_perimeter(grid):
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
