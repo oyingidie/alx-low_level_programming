3f11614634a3ceca5113b1bea239b128899d705f
oyingidie <oyinkansoladebayo@gmail.com>
2024-04-06 00:16:08 -0700

    Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees memory occupied by a doubly linked list
+ * @head: Pointer to the head of the list
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((tmp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
+}
