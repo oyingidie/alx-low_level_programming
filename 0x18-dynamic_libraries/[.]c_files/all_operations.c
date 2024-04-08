de0e370952a0b56006cb2c3ac08bbf74fb21f945
oyingidie <oyinkansoladebayo@gmail.com>
2024-04-07 21:28:14 -0700

    Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/[.]c_files/all_operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - Display the sum of two integers
+ * @x: First integer
+ * @y: Second integer
+ *
+ * Return: Result of the operation
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+/**
+ * sub - Display the difference of two integers
+ * @x: First integer
+ * @y: Second integer
+ *
+ * Return: Result of the operation
+ */
+int sub(int x, int y)
+{
+	return (x - y);
+}
+
+/**
+ * mul - Display the product of two integers
+ * @x: First integer
+ * @y: Second integer
+ *
+ * Return: Result of the operation
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+
+/**
+ * div - Display the quotient of two integers
+ * @x: First integer
+ * @y: Second integer
+ *
+ * Return: Result of the operation
+ */
+int div(int x, int y)
+{
+	return (x / y);
+}
+
+/**
+ * mod - Display the remainder of two integers
+ * @x: First integer
+ * @y: Second integer
+ *
+ * Return: Result of the operation
+ */
+int mod(int x, int y)
+{
+	return (x % y);
+}
