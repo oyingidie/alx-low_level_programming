68ddfa2bd091c1593f1a06d852e14c526c593451
oyingidie <oyinkansoladebayo@gmail.com>
2024-05-04 03:18:04 -0700

    Heady as they get

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,8 +1,10 @@
 #ifndef _HASH_TABLES_H_
 #define _HASH_TABLES_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -39,4 +41,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
 #endif
